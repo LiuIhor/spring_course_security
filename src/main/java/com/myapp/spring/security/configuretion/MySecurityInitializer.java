58786b5cf1611dcb553c9c298a5cdf4a6d1a1c9d
spaceboy4207 <ighor.liu99@gmail.com>
2022-01-13 11:05:28 +0200

    Initial commit

---
+++ src/main/java/com/myapp/spring/security/configuretion/MySecurityInitializer.java
@@ -0,0 +1,6 @@
+package com.myapp.spring.security.configuretion;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MySecurityInitializer extends AbstractSecurityWebApplicationInitializer {
+}
